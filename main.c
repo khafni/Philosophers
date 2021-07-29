627d711af435ed7e2fde7063903deb1910b2b425
pizzaboi <hafnix@gmail.com>
2021-07-29 14:53:43 +0100

    ok

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+
+void *routine()
+{
+	printf("test from thread\n");
+}
+
+int main(int argc, char **argv)
+{
+	pthread_t t1;
+
+	pthread_create(&t1, )
+	return (0);
+}
